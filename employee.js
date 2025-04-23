457b01fea6b72c7fc05e89eb1395fb2cfd62f70e
Shakirzaman007 <shakir071096@gmail.com>
2025-04-23 12:46:28 +0600

    css

--- employee.js
+++ employee.js
@@ -3,8 +3,7 @@
     const today = new Date();
     let age = today.getFullYear() - birthDate.getFullYear();
     const m = today.getMonth() - birthDate.getMonth();
-    if (
-      m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
+    if ( m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
        {
       age--;
        }
